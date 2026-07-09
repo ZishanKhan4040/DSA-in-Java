fa3035b4a6bc9ed0c7f3baebb909b476d5fdba4c
Zishan <147532051+ZishanKhan4040@users.noreply.github.com>
2026-07-09 23:27:56 +0530

    Implement string length calculation
    Fix typo in print statement and correct exception handling.

---
+++ String/CalculateLengthOfString.java
@@ -0,0 +1,19 @@
+public class LengthOfString{
+
+  public static void main(String [] args){
+String str="Hello World";
+    try{
+      int i=0;
+      while(true){
+        str.charAt(i);
+        i++;
+      }
+    }
+    catch(Exception e){
+Systen.out.println("Length of String "+str+" "+i);
+      
+    }
+    
+  }
+  
+}
