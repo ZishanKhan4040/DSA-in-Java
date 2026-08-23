41cfb238b46ebc544789a0c6d16e73e6ad61e7d5
Zishan <147532051+ZishanKhan4040@users.noreply.github.com>
2026-08-23 23:46:12 +0530

    Create MultiplyStrings.java

---
+++ MultiplyStrings.java
@@ -0,0 +1,44 @@
+
+class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0"))
+            return "0";
+
+        int n = num1.length();
+        int m = num2.length();
+
+        int[] result = new int[n + m];
+
+        // Multiply each digit
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = product + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        // Convert array to string
+        StringBuilder ans = new StringBuilder();
+
+        for (int digit : result) {
+            if (ans.length() == 0 && digit == 0)
+                continue;
+
+            ans.append(digit);
+        }
+
+        return ans.toString();
+    }
+}
