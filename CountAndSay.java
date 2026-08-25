0874be3ff6a839c628248553a01b0e803f25500c
Zishan <147532051+ZishanKhan4040@users.noreply.github.com>
2026-08-25 23:45:10 +0530

    Create CountAndSay.java

---
+++ CountAndSay.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+
+        for (int i = 2; i <= n; i++) {
+            StringBuilder next = new StringBuilder();
+
+            int count = 1;
+
+            for (int j = 1; j < s.length(); j++) {
+
+                if (s.charAt(j) == s.charAt(j - 1)) {
+                    count++;
+                } else {
+                    next.append(count);
+                    next.append(s.charAt(j - 1));
+                    count = 1;
+                }
+            }
+
+            // Add the last group
+            next.append(count);
+            next.append(s.charAt(s.length() - 1));
+
+            s = next.toString();
+        }
+
+        return s;
+    }
+}
